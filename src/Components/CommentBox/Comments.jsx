917316d3e10d7e8b8a58ac5804d311d26c132491
Afaq ali <codergithub591@gmail.com>
2026-09-13 15:25:24 +0500

    Comment styling is done

--- src/Components/CommentBox/Comments.jsx
+++ src/Components/CommentBox/Comments.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import { CornerUpLeft, Ellipsis, ThumbsUp, User } from "lucide-react";
 
 const Comments = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="flex flex-col gap-4 my-4">
+        <div className="h-25 mx-6 bg-cardColor shadow-lg border border-borderColor rounded-2xl">
+          <div className="flex items-center justify-between px-6 py-2">
+            <div className="flex items-center gap-4">
+              <User size={40} />
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-2 font-medium">
+                  <h1>User</h1>
+                  <p className="text-bodyTextColor text-sm">2 hours</p>
+                </div>
 
-export default Comments
+                <p className="font-medium text-sm te">
+                  This is great post.Really enjoyed reading it.
+                </p>
+                {/* icons */}
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <span className="flex items-center gap-2">
+                    <ThumbsUp size={20} /> 10{" "}
+                  </span>
+                  <span className="flex items-center gap-2">
+                    reply <CornerUpLeft size={20} />
+                  </span>
+                </div>
+              </div>
+            </div>
+            <Ellipsis size={20} className="cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
+export default Comments;
